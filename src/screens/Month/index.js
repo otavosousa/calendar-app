e8710b0869cad70d6d7cde67b830bc851810afd6
otavio-sousa <otavioklearskin@gmail.com>
2021-07-23 19:55:41 -0300

    feat:  created month calendar UI screen using the table component and getting navigation between screens

--- src/screens/Month/index.js
+++ src/screens/Month/index.js
@@ -1,10 +1,53 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import styles from './styles'
+import FactoryMonth  from '../../factories/FactoryMonth'
+import { Table, Row } from 'react-native-table-component';
+import { useNavigation } from '@react-navigation/native'
 
 function Month(){
+    
+    // HOOKS
+    const navigation = useNavigation()
+
+    // FACTORY
+    const factoryMonth = new FactoryMonth()
+
+    // HANDLES
+
+    const handleToPageDay = (value) => {
+
+        navigation.navigate('Day', {
+            day: value
+        })
+
+    }
+
+    const handleSelectDay = (value) => (
+        <TouchableOpacity onPress={() => handleToPageDay(value)}>
+          <View >
+            <Text style={styles.calendarBodyText} >{value}</Text>
+          </View>
+        </TouchableOpacity>
+      );
 
     return(
-        <View />
+        
+        <View style={styles.container}>
+            <View>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.titleText}>{factoryMonth.getName()}</Text>
+                </View>
+                <View>
+                    <Table>
+                        <Row data={factoryMonth.getDaysName()} style={styles.calendarHead} textStyle={styles.calendarHeadText}/>
+                        {factoryMonth.getDaysNumber().map((months, index) => (
+                            <Row data={months.map(month => month != "" ? handleSelectDay(month) : month )} key={String(index)}/>
+                        ))}
+                    </Table>
+                </View>
+            </View>
+        </View>
     )
 }
 
---
+++ src/screens/Month/styles.js
@@ -0,0 +1,47 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+const styles = StyleSheet.create({
+    container: {
+     flex: 1,
+     paddingTop: Constants.statusBarHeight,
+     padding: 20
+    },
+
+    titleContainer: {
+        paddingBottom: 20,
+        borderColor: 'lightgray',
+        borderBottomWidth: 1
+    },
+    titleText: {
+
+        color: '#535353',
+        fontWeight: 'bold',
+        fontSize: 30,
+        textTransform: 'capitalize',
+    },
+
+    calendarHead: {
+        marginTop: 10,
+        marginBottom: 10,
+        paddingBottom: 10,
+        borderBottomWidth: 1,
+        borderColor: 'lightgray'
+    },
+
+    calendarHeadText: {
+        padding: 10,
+        textAlign: 'center',
+        color: '#535353',
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    calendarBodyText: {
+        padding: 10,
+        textAlign: 'center',
+        fontSize: 20,
+        marginBottom: 40
+    },
+})
+
+export default styles
