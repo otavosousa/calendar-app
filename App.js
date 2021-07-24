606673045605f06e9539edf5599fdbae7dfaf285
otavio-sousa <otavioklearskin@gmail.com>
2021-07-24 14:56:35 -0300

    feat: config provider component above routes component

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Routes from './src/routes'
+import { Provider } from 'react-redux'
+import store from './src/store'
 
 export default function App() {
   return (
-    <Routes />
+    <Provider store={store}>
+        <Routes />
+    </Provider>
   );
 }
