6c0b7d6299afde1525daf659ee793d40df2a79a3
otavio-sousa <otavioklearskin@gmail.com>
2021-07-25 15:27:53 -0300

    feat: created a factory to get data to response API Weather and translate method weather name to Portuguese

---
+++ src/factories/FactoryWeather.js
@@ -0,0 +1,51 @@
+import { WEATHER_API_KEY } from "@env"
+
+function FactoryWeather(){
+    const weather = {}
+
+    function get(city){ 
+
+        const response = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&cnt=7&appid=${WEATHER_API_KEY}`)
+            .then(response => {
+                if(!response.ok) {
+                    throw new Error('No references')
+                }
+                return response.json()
+            })
+            .then(data => {
+                if(!data) {
+                    return 'erro'
+                }
+                return data
+            });
+
+        return response
+    }
+
+    function translate(city) {
+
+        const name = {
+
+            Thunderstorm: 'Trovoada',
+            Drizzle: 'Chuviscando',
+            Rain: 'Chuva',
+            Snow: 'Nevando',
+            Clear: 'Ensolarado',
+            Clouds: 'Nublado'
+        }
+
+        if(!name[city]) {
+            return 'Trovoada' 
+        }
+
+        return name[city]
+
+    }
+    
+    weather.get = get
+    weather.translate = translate
+
+    return weather
+}
+
+export default FactoryWeather
