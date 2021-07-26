7d51486af35d520f1122e81452c0c7e33d599777
otavio-sousa <otavioklearskin@gmail.com>
2021-07-25 21:09:12 -0300

    feat: add edit and delete action

--- src/reducers/remindersReducer.js
+++ src/reducers/remindersReducer.js
@@ -8,6 +8,10 @@ function reminders(state=INITIAL_STATE, action) {
         
         case 'ADD_REMINDER': 
             return {...state, data: [...state.data, action.data ]}
+        case 'EDIT_REMINDER': 
+            return {...state, data: [...state.data.filter(reminder => reminder.id !== action.data.id), action.data ]}
+        case 'DELETE_REMINDER': 
+            return {...state, data: [...state.data.filter(reminder => reminder.id !== action.data.id)]}
         default:
             return state
     }
