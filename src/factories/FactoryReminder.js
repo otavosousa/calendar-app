7907b36c80d50d212a844dd2d8ce5bff0d074f7a
otavio-sousa <otavioklearskin@gmail.com>
2021-07-25 21:17:54 -0300

    feat: created methods that run processes on Async Storage and actions to handle the state of reminders.

--- src/factories/FactoryReminder.js
+++ src/factories/FactoryReminder.js
@@ -5,7 +5,6 @@ function FactoryReminder(){
 
     const reminder = {}
     const dispatch = useDispatch()
-    const store = useSelector(state => state.reminder.data)
 
     async function get(){
         let reminders = []
@@ -29,14 +28,65 @@ function FactoryReminder(){
         actionAdd(data)
     }
 
+    async function edit(data){
+
+        const response = await AsyncStorage.getItem('@calendar-reminders')
+
+        if(response) {
+
+            const responseData = JSON.parse(response)
+            const reminders = responseData.filter(reminder => reminder.id !== data.id)
+            await AsyncStorage.setItem('@calendar-reminders', JSON.stringify([...reminders, data]))
+            actionEdit(data)
+        }
+    }
+
+    async function del(data){
+
+        const response = await AsyncStorage.getItem('@calendar-reminders')
+
+        if(response) {
+            const responseData = JSON.parse(response)
+            const reminders = responseData.filter(reminder => reminder.id !== data.id)
+            await AsyncStorage.setItem('@calendar-reminders', JSON.stringify([...reminders]))
+            actionDelete(data)
+        }
+    }
+
     function actionAdd(data) {
 
         return dispatch({type: 'ADD_REMINDER', data})
     }
 
+    function actionEdit(data) {
+
+        return dispatch({type: 'EDIT_REMINDER', data })
+    }
+
+    function actionDelete(data) {
+
+        return dispatch({type: 'DELETE_REMINDER', data })
+    }
+
+    function actionCache(data) {
+
+        return dispatch({type: 'CACHE_REMINDER', data })
+    }
+
+    function actionReset() {
+
+        return dispatch({type: 'RESET_REMINDER'})
+    }
+
     reminder.get = get
     reminder.set = set
+    reminder.edit = edit
+    reminder.del = del
     reminder.actionAdd = actionAdd
+    reminder.actionEdit = actionEdit
+    reminder.actionCache = actionCache
+    reminder.actionDelete = actionDelete
+    reminder.actionReset = actionReset
 
     return reminder
 }
