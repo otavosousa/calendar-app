a9f8fe0c6c7d02b44c48d144c7b529ce5746de6b
otavio-sousa <otavioklearskin@gmail.com>
2021-07-25 21:08:36 -0300

    fix: renamed action from reducer reminder and altered initial state

--- src/reducers/reminderReducer.js
+++ src/reducers/reminderReducer.js
@@ -3,7 +3,7 @@ const INITIAL_STATE = {
         id: '',
         title: '', 
         start: '00:00', 
-        finish: '01:00', 
+        finish: '00:00', 
         local: '', 
         color : '#3498DB',
         day: '',
@@ -15,14 +15,14 @@ function reminder(state=INITIAL_STATE, action) {
 
     switch(action.type) {
         
-        case 'EDIT_REMINDER': 
+        case 'CACHE_REMINDER': 
             return {...state, data: Object.assign(state.data, action.data)}
         case 'RESET_REMINDER': 
             return {...state, data: { 
                 id: '',
                 title: '', 
                 start: '00:00', 
-                finish: '01:00', 
+                finish: '00:00', 
                 local: '', 
                 color : '#3498DB',
                 day: '',
